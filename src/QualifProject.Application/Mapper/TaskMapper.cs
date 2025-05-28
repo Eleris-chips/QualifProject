4bb98490350645f2dfc07e02c64baddaa5812791
Vilcot Vincent (ENGIE SA) <LC5581@engie.com>
2025-05-28 18:37:53 +0200

    Mapper

---
+++ src/QualifProject.Application/Mapper/TaskMapper.cs
@@ -0,0 +1,19 @@
+﻿using QualifProject.Application.Models;
+using QualifProject.Domain.Task;
+
+namespace QualifProject.Application.Mapper;
+
+public static class TaskMapper
+{
+    #region Public Methods
+
+    /// <summary>
+    /// Map a task aggregate to a dto.
+    /// </summary>
+    /// <param name="entity">The entity to map.</param>
+    /// <returns>A Task Dto.</returns>
+    public static TaskDto ToDto(this TaskAggregate entity)
+    => throw new NotImplementedException();
+
+    #endregion Public Methods
+}
---
+++ tests/QualifProject.Tests/TaskMapperTests.cs
@@ -0,0 +1,26 @@
+﻿using NFluent;
+using QualifProject.Application.Mapper;
+using QualifProject.Domain.Task;
+
+namespace QualifProject.Tests;
+
+[TestClass]
+public class TaskMapperTests
+{
+    #region Public Methods
+
+    [TestMethod]
+    public void GivenTaskAggregateWhenToDtoThenTaskDto()
+    {
+        var task = new TaskAggregate("MyTask", false, "My task");
+        task.SetId(1);
+
+        var taskDto = task.ToDto();
+
+        Check.That(taskDto.Id).IsEqualTo(task.Id);
+        Check.That(taskDto.Infos.Description).IsEqualTo(task.Description);
+        Check.That(taskDto.Infos.Title).IsEqualTo(task.Title);
+    }
+
+    #endregion Public Methods
+}
